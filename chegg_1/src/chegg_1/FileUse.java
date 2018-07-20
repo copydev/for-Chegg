80b66bf8fc018bdce7bfc80c54ad7918d32e0dc6
USER <USER@192.168.99.1>
2018-07-20 13:24:28 +1100

    New Repo_Via_Eclipse

---
+++ chegg_1/src/chegg_1/FileUse.java
@@ -0,0 +1,35 @@
+package chegg_1;
+
+import java.io.File;
+
+public class FileUse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = new String("C:\\Users\\USER\\Desktop\\test");
+		System.out.println(fileCount(str));
+
+	}
+	
+	public static int fileCount(String root){
+		File start = new File(root);
+		if(start.list().length==0){
+			return 0;
+		}
+		
+		int count = 0;
+		File[] arrayOfFiles = start.listFiles();
+		for(File found: arrayOfFiles){
+			if(found.isFile()){
+				count++;
+			}
+			else{
+				String folderRoot = found.getPath();
+				count+=fileCount(folderRoot);
+			}
+		}
+		
+		return count;
+	}
+
+}
